42a3cb9f5272df6fbbb30ec2f28da1ae4b9aaf09
blue_lobster <42709757+vincent-tvincent@users.noreply.github.com>
2021-12-10 17:55:34 -0500

    move expressions to project

---
+++ Operator.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Operator implements CompoundExpression {
+    private String _identifier;
+    private Operator _parent;
+    private List<Expression> _subexpresions;
+
+    public Operator (String identifier) {
+        _identifier = identifier;
+        _subexpresions = new ArrayList<>();
+    }
+
+    public String getIdentifier() {
+        return _identifier;
+    }
+
+    @Override
+    public void addSubexpression(Expression subexpression) {
+        _subexpresions.add(subexpression);
+    }
+
+    public void removeSubexpression(Expression subexpression) {
+        _subexpresions.remove(subexpression);
+    }
+
+    public CompoundExpression getParent() {
+        return _parent;
+    }
+
+    public void setParent(CompoundExpression parent) {
+        _parent = (Operator) parent;
+    }
+
+    public Expression deepCopy() {
+        Operator op = new Operator(_identifier);
+        op.setParent(_parent);
+
+        Expression[] subArray = new Expression[_subexpresions.size()];
+        _subexpresions.toArray(subArray);
+
+        for (int i = 0; i < subArray.length; i++) {
+            op.addSubexpression(subArray[i]);
+        }
+        return op;
+    }
+
+    /*
+     * removes redundant operators
+     */
+    public void flatten() {
+        Expression[] subArray = new Expression[_subexpresions.size()];
+        _subexpresions.toArray(subArray);
+
+        for (int i = 0; i < subArray.length; i++) {
+            subArray[i].flatten();
+        }
+
+        if(_parent != null && _parent.getIdentifier().equals(_identifier)) {
+            for (int i = 0; i < subArray.length; i++) {
+                _parent.addSubexpression(subArray[i]);
+                subArray[i].setParent(_parent);
+
+                _parent.removeSubexpression(this);
+            }
+        }
+    }
+
+    @Override
+    public void setParent(Operator parent) {
+
+    }
+
+    @Override
+    public double evaluate(double x) {
+        return 0;
+    }
+
+    /*
+     * Flattens the tree then prints it
+     */
+    public String convertToString(int indentLevel) {
+        // flatten before convert
+        flatten();
+        StringBuilder s = new StringBuilder();
+        Expression.indent(s, indentLevel);
+        s.append(_identifier);
+
+        indentLevel += 1;
+        for(Expression e : _subexpresions) {
+            s.append("\n");
+            s.append(e.convertToString(indentLevel));
+        }
+        if(_parent == null) {
+            s.append("\n");
+        }
+        return s.toString();
+    }
+}
